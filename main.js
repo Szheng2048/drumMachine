0d9b4b5007ff7a533e84cfd3ab8ff28dad9a1d40
Steven Zheng <szheng.persevere@gmail.com>
2024-02-06 11:23:04 -0600

    made changes to the tick tock play sound so that each fourth interval tock is played and any of the four other sounds are played

--- main.js
+++ main.js
@@ -2,14 +2,19 @@
 const tick = new Audio('sounds/tick.mp3');
 const tock = new Audio("sounds/tock.mp3");
 let count = 0
+const countElement = document.createElement("p")
+const countOnScreen = document.querySelector(".tip")
+countOnScreen.appendChild(countElement)
 // This function is called every 600ms
 function update() {
     // Play the 'tick' sound
     count ++
     if(count % 4 === 0){
         tock.play()
+        countElement.innerText = 4
     } else {
         tick.play()
+        countElement.innerText = `${count%4}`
     }
 }
 
